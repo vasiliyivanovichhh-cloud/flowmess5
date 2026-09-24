db65600c37620edf96737d3e59044610ffe0acdc
vasiliyivanovichhh-cloud <vasiliyivanovichhh@gmail.com>
2026-09-24 19:17:20 +0500

    Rebuild Flow Messenger from scratch

--- MessengerApp/RootView.swift
+++ MessengerApp/RootView.swift
@@ -2,16 +2,9 @@ import SwiftUI
 
 struct RootView: View {
     @EnvironmentObject private var session: AppSession
-
     var body: some View {
         Group {
-            if session.currentUser == nil {
-                AuthView()
-            } else {
-                MainTabView()
-            }
-        }
-        .animation(.easeInOut, value: session.currentUser)
+            if session.currentUser == nil { AuthView() } else { MainTabView() }
+        }.animation(.easeInOut(duration: 0.2), value: session.currentUser)
     }
 }
-
